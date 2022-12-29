f7a87b705d9005e7017d97997fc5e3e77f378038
Rutvijthummar <90086647+Rutvijthummar@users.noreply.github.com>
2022-12-29 18:25:12 +0530

    Add files via upload

---
+++ Module-2/M2ASS24/M2ASS24/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  M2ASS24
+//
+//  Created by MAC on 29/12/22.
+//  WAP to delete value from array at specific position.
+
+import Foundation
+
+var fruits = ["apple", "banana", "cherry", "mango", "guava"]
+var index = 2
+fruits.remove(at: index)
+print(fruits)
+

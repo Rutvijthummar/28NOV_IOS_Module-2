bae2f63792e9b93a37f2ee73a85dbee67c853cbd
Rutvijthummar <90086647+Rutvijthummar@users.noreply.github.com>
2022-12-29 18:21:41 +0530

    Add files via upload

---
+++ Module-2/M2ASS2/M2ASS2/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  M2ASS2
+//
+//  Created by MAC1 on 28/12/22.
+// WAP to take an integer value from user.
+
+import Foundation
+
+print("Enter your name")
+if
+    let yourname = readLine()
+{
+    print("Hello your name is \(yourname)")
+}
+
---
+++ Module-2/M2ASS9/M2ASS9/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  M2ASS9
+//
+//  Created by MAC1 on 28/12/22.
+// WAP to make variable optional and requirements if variable
+// have not value then it doesn't show an error.
+
+import Foundation
+
+var a : Int?
+print(a)
+
+var b = readLine()
+print(b)
+

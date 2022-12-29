0dcc3a88e6092a8572435f27c9e327160fe6cace
Rutvijthummar <90086647+Rutvijthummar@users.noreply.github.com>
2022-12-29 17:48:10 +0530

    Add files via upload

---
+++ M2ASS14/M2ASS14/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  M2ASS14
+//
+//  Created by MAC1 on 28/12/22.
+// WAP to perform operation like. Union, intersection etc. using
+// set.
+
+import Foundation
+
+var setname: Set<String> = [ "a", "b", "c", "d", "e"]
+
+print(setname)
+
---
+++ M2ASS15/M2ASS15/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  M2ASS15
+//
+//  Created by MAC on 29/12/22.
+//  WAP to make array of dictionaries and get a value from specific dictionaries.
+
+import Foundation
+
+var names: [String: String] = [
+    "india": "Delhi",
+    "srilanka": "Colombo",
+    "england": "London"
+]
+
+for (key, value) in names {
+    print("Key: \(key) -------- Value: \(value)")
+}
+

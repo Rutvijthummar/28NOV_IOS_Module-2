a539698683311d4a19866afc3c29d45810194420
Rutvijthummar <90086647+Rutvijthummar@users.noreply.github.com>
2022-12-29 17:52:37 +0530

    Add files via upload

---
+++ M2ASS22/M2ASS22/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  M2ASS22
+//
+//  Created by MAC on 29/12/22.
+//  WAP to sort string array.
+
+import Foundation
+
+var students = ["Kofi", "Abena", "Peter", "Kweku", "Akosua"]
+students.sort()
+print(students)
+
---
+++ M2ASS23/M2ASS23/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  M2ASS23
+//
+//  Created by MAC on 29/12/22.
+//  WAP to add new value at the last position of array.
+
+import Foundation
+
+var numbers = [21, 34, 54, 12]
+numbers.insert(20, at: numbers.endIndex)
+print(numbers)
+
